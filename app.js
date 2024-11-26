5d471bc278d98734f85950d3ae910846a0c688f0
Michael Innella <mainnella@gmail.com>
2024-11-26 10:03:35 -0500

    Updates
    added modal, improved btn menu, cleaned up HTML and CSS structures

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Btn Menu
+
 function openMenu() {
   document.body.classList.add("menu--open");
 }
@@ -5,3 +7,33 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu--open");
 }
+
+//Modal
+
+//Elements
+
+const modal = document.getElementById("contactModal");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const body = document.body;
+
+//Open Modal
+
+openModalBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+  body.classList.add("no-scroll");
+});
+
+//Close Modal
+
+closeModalBtn.addEventListener("click", (event) => {
+  modal.style.display = "none";
+  body.classList.remove("no-scroll");
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    body.classList.remove("no-scroll");
+  }
+});
